5579fad29e3c8a602e521e83801be3a932fd3f8a
SulRash <sultan.m.rashed@gmail.com>
2024-04-24 14:12:32 +0000

    Updated baby model finetuning and training

--- config/finetune_ntafl_baby.py
+++ config/finetune_ntafl_baby.py
@@ -1,8 +1,8 @@
 import time
 
 out_dir = 'out-baby-ntagent-frozenlake'
-eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
+eval_interval = 50 # keep frequent because we'll overfit
+eval_iters = 20
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -11,6 +11,8 @@ always_save_checkpoint = False
 wandb_log = False # override via command line if you like
 wandb_project = 'ntagent'
 wandb_run_name = 'baby-ntagent-finetune-fl-'+str(time.time())
+
+init_from = 'resume'
 
 dataset = 'frozenlake_ft'
 gradient_accumulation_steps = 1
@@ -23,11 +25,11 @@ n_head = 6
 n_embd = 384
 dropout = 0.2
 
-learning_rate = 3e-5 # with baby networks can afford to go a bit higher
-max_iters = 5000
+learning_rate = 5e-5 # with baby networks can afford to go a bit higher
+max_iters = 3500
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+# warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
 device = 'cuda'  # run on cpu only
--- config/train_ntafl_baby.py
+++ config/train_ntafl_baby.py
@@ -1,8 +1,8 @@
 import time
 
 out_dir = 'out-baby-ntagent-frozenlake'
-eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
+eval_interval = 50 # keep frequent because we'll overfit
+eval_iters = 20
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -24,8 +24,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 10000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 2000
+lr_decay_iters = 2000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
