5d34d21dd4ee73cc59ad7f8b28fa864c6dde9287
Sultan <sultan.m.rashed@gmail.com>
2024-04-24 10:34:19 +0300

    Rewrote some config files

--- config/train_ntafl.py
+++ config/train_ntafl.py
@@ -1,14 +1,15 @@
-# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
-# launch as the following (e.g. in a screen session) and wait ~5 days:
-# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+import time
 
+out_dir = 'out-ntagent-frozenlake'
 wandb_log = True
-wandb_project = 'owt'
-wandb_run_name='gpt2-124M-fl'
+wandb_project = 'ntagent'
+wandb_run_name='ntagent-fl-'+str(time.time())
+
+dataset = 'frozenlake'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
+batch_size = 4
 block_size = 1024
 gradient_accumulation_steps = 5 * 8
 
