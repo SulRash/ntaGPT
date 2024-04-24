3c95401bd5e44211fb904e4c85f772513ad95a8a
Sultan <sultan.m.rashed@gmail.com>
2024-04-24 11:23:50 +0300

    Added tokenizer and detokenizer to file to avoid relative imports

--- data/frozenlake/prepare.py
+++ data/frozenlake/prepare.py
@@ -2,7 +2,37 @@ import os
 import pickle
 import numpy as np
 
-from data.frozen_lake_data import tokenizer, detokenizer
+# Map-to-Token to avoid the tokenization step.
+# 0, 1, and 2 are reserved for <eor>, <eom>, and \n respectively
+MTT = {
+    "S": "3",
+    "G": "4",
+    "F": "5",
+    "H": "6"
+}
+
+TTM = {
+    0: "<eor>",
+    1: "<eom>",
+    2: "\n",
+    3: "S",
+    4: "G",
+    5: "F",
+    6: "H"
+}
+
+def tokenizer(string: str):
+    string = string.replace("<eor>", "0")
+    string = string.replace("<eom>", "1")
+    string = string.replace("\n", "2")
+    string = string.replace("S", MTT["S"])
+    string = string.replace("G", MTT["G"])
+    string = string.replace("F", MTT["F"])
+    string = string.replace("H", MTT["H"])
+    return list(map(int, list(string)))
+
+def detokenizer(ids: List[int]):
+    ''.join([TTM[i] for i in ids])
 
 # Use the pretraining data
 input_file_path = '/Users/sultan/Documents/Github/RLALLM/data/generated_data/pt_data.txt'
