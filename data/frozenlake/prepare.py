00b5c7dd3425139e9d350b644fb2624c9c3699a1
SulRash <sultan.m.rashed@gmail.com>
2024-04-25 09:20:57 +0000

    Moved tokenizer and detokenizer outside of preparation file

--- data/frozenlake/prepare.py
+++ data/frozenlake/prepare.py
@@ -3,37 +3,7 @@ import pickle
 import numpy as np
 from argparse import ArgumentParser
 
-# Map-to-Token to avoid the tokenization step.
-# 0, 1, and 2 are reserved for <eor>, <eom>, and \n respectively
-MTT = {
-    "S": "3",
-    "G": "4",
-    "F": "5",
-    "H": "6"
-}
-
-TTM = {
-    0: "<eor>",
-    1: "<eom>",
-    2: "\n",
-    3: "S",
-    4: "G",
-    5: "F",
-    6: "H"
-}
-
-def tokenizer(string: str):
-    string = string.replace("<eor>", "0")
-    string = string.replace("<eom>", "1")
-    string = string.replace("\n", "2")
-    string = string.replace("S", MTT["S"])
-    string = string.replace("G", MTT["G"])
-    string = string.replace("F", MTT["F"])
-    string = string.replace("H", MTT["H"])
-    return list(map(int, list(string)))
-
-def detokenizer(ids):
-    ''.join([TTM[i] for i in ids])
+from utils.frozenlake import tokenizer
 
 parser = ArgumentParser()
 parser.add_argument('--data_path', type=str)
